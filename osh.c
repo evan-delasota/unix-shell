1e63f81ff1f59603b2e94b47a4e573f297cdaee1
evan-delasota <evan.delasota@gmail.com>
2021-03-24 17:50:48 -0600

    Replaced parse  with tokenize, refactored main

--- osh.c
+++ osh.c
@@ -1,57 +1,117 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
-void parse(char* s) {
-  const char break_chars[] = " \t";
-  char* p;
-  p = strtok(s, break_chars);
-  while (p != NULL) {
-    printf("token was: %s\n", p);
-    p = strtok(NULL, break_chars);
+#define DELIM "\t\r\n\a"
+#define BUFSZ 64
+
+char** tokenize(char* s) {
+  int i = 0;
+  int buffer = BUFSZ;
+  char** tokens = malloc(buffer * sizeof(char*));
+  char* token;
+
+  token = strtok(s, DELIM);
+  while (token != NULL) {
+    tokens[i] = token;
+    i++;
+
+    if (i >= buffer) {
+      buffer += BUFSZ;
+      tokens = realloc(tokens, buffer * sizeof(char*));
+    }
+
+  token = strtok(NULL, DELIM);
   }
+
+  tokens[i] = NULL;
+  return tokens;
 }
 
-// test
 int main(int argc, const char * argv[]) {  
-  char input[BUFSIZ];
-  char last_command[BUFSIZ];
-  
-  memset(input, 0, BUFSIZ * sizeof(char));
-  memset(input, 0, BUFSIZ * sizeof(char));
+  char* input = NULL;
+  char* last_command;
+  char** args;
+  char* ioType[4] = {"<", ">", "|", "&"};
+  int ioSymbol = 0;
+  int select;
+  bool isCommandOrFgProc;
+
   bool finished = false;
   
   while (!finished) {
+    isCommandOrFgProc = false;
+    ssize_t sz = 0;
+
     printf("osh> ");
     fflush(stdout);
+    getline(&input, &sz, stdin);
+    args = tokenize(input);
 
-    if ((fgets(input, BUFSIZ, stdin)) == NULL) {   // or gets(input, BUFSIZ);
-      fprintf(stderr, "no command entered\n");
-      exit(1);
+    if (strncmp(args[0], "!!", 2) != 0) {
+      last_command = strdup(args[0]);
     }
-    input[strlen(input) - 1] = '\0';          // wipe out newline at end of string
+
+    if (args[0] == NULL) {
+      free(input);
+      free(args);
+      continue;
+    }
     printf("input was: \n'%s'\n", input);
 
+    if (strcmp(args[0], "exit") == 0) {
+      finished = true;
+    }
     // check for history (!!) command
-    if (strncmp(input, "!!", 2) == 0) {
+    if (strncmp(args[0], "!!", 2) == 0) {
       if (strlen(last_command) == 0) {
         fprintf(stderr, "no last command to execute\n");
+      } else {
+        printf("last command was: %s\n", last_command);
       }
-      printf("last command was: %s\n", last_command);
-    } else if (strncmp(input, "exit", 4) == 0) {   // only compare first 4 letters
+    } else if (strncmp(args[0], "exit", 4) == 0) {   // only compare first 4 letters
       finished = true;
     } else {
-      strcpy(last_command, input);
-      printf("You entered: %s\n", input);   // you will call fork/exec
-      parse(input);
-			printf("after parse, what is input: %s\n", input);
-      printf("\n");
+      select = 1;
+      while (args[select] != NULL) {
+        for (ioSymbol = 0; ioSymbol < 4; ioSymbol++) {
+          if (strcmp(args[select], ioType[ioSymbol]) == 0) {
+            break;
+          }
+        }
+        
+        if (ioSymbol < 4) {
+          isCommandOrFgProc = true;
+
+          if (ioSymbol < 2) {
+            //ioRedir(args, select, ioSymbol);
+          } else if (ioSymbol == 2) {
+            //pipeRedir(args, select);
+          } else if (ioSymbol == 3) {
+            //initProc(args, select);
+          }
+          break;
+        }
+        select++;
+
+      }
+      if (!isCommandOrFgProc) {
+        initProc(args, 0);
+      }
+
+      free(args);
+      free(input);
     }
   }
   
   printf("osh exited\n");
   printf("program finished\n");
-  
   return 0;
 }
